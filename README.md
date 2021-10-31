98e07539648ced7deb44e9872c3004bf5b61110b
quarantin <codelorme@gmail.com>
2021-10-31 19:55:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# zomboid-quick-item-action
+A mod for the game Project Zomboid to add missing actions when you double-click some items, like books, cigarettes, pills, and keys
---
+++ media/lua/client/QuickItemAction/QuickItemAction.lua
@@ -0,0 +1,40 @@
+
+local origDoContextualDblClick = ISInventoryPane.doContextualDblClick;
+
+function ISInventoryPane:doContextualDblClick(item)
+
+	-- Books
+    if item:getCategory() == 'Literature' then
+		ISInventoryPaneContextMenu.readItem(item, 0)
+    end
+
+	-- Pills
+	if instanceof(item, 'Drainable') and luautils.stringStarts(item:getType(), 'Pills') then
+		ISInventoryPaneContextMenu.takePill(item, 0)
+	end
+
+	-- Cigarettes
+	if item:getName() == 'Cigarettes' then
+		ISInventoryPaneContextMenu.eatItem(item, 1, 0)
+	end
+
+	-- Umbrellas
+	if item:getName() == 'Umbrella' then
+		ISInventoryPaneContextMenu.equipWeapon(item, false, false, 0)
+	end
+
+	-- Keys
+    if item:getCategory() == 'Key' then
+		local player = getPlayer()
+		local containers = player:getInventory():getItemsFromCategory('Container')
+		for i = 0, containers:size() - 1 do
+			local container = containers:get(i)
+			if player:isEquipped(container) and container:getType() == 'KeyRing' then
+				container:getInventory():addItem(item)
+				break
+			end
+		end
+    end
+
+    return origDoContextualDblClick(self, item);
+end
