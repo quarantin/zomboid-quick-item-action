1ec8da847c6240559d3578439cb40ce42ae7b6dc
Corentin Delorme <codelorme@gmail.com>
2021-11-19 20:36:13 +0100

    Update QuickItemAction.lua: Refactoring: Use item:getScriptItem():getTypeString() to retrieve the item category instead of item:getCategory() which does not work for all items + Use elseif statements

--- media/lua/client/QuickItemAction/QuickItemAction.lua
+++ media/lua/client/QuickItemAction/QuickItemAction.lua
@@ -3,19 +3,18 @@ local origDoContextualDblClick = ISInventoryPane.doContextualDblClick;
 
 function ISInventoryPane:doContextualDblClick(item)
 
+	local player = getPlayer()
+
 	-- Books
-	if item:getCategory() == 'Literature' then
+	if item:getScriptItem():getTypeString() == 'Literature' then
 		ISInventoryPaneContextMenu.readItem(item, 0)
-	end
 
 	-- Cigarettes
-	if item:getName() == 'Cigarettes' then
+	elseif item:getName() == 'Cigarettes' then
 		ISInventoryPaneContextMenu.eatItem(item, 1, 0)
-	end
 
 	-- Keys
-	if item:getCategory() == 'Key' then
-		local player = getPlayer()
+	elseif item:getScriptItem():getTypeString() == 'Key' then
 		local containers = player:getInventory():getItemsFromCategory('Container')
 		for i = 0, containers:size() - 1 do
 			local container = containers:get(i)
@@ -24,20 +23,17 @@ function ISInventoryPane:doContextualDblClick(item)
 				break
 			end
 		end
-	end
 
 	-- Maps
-	if luautils.stringEnds(item:getType(), 'Map') then
+	elseif luautils.stringEnds(item:getType(), 'Map') then
 		ISInventoryPaneContextMenu.onCheckMap(item, 0)
-	end
 
 	-- Pills
-	if instanceof(item, 'Drainable') and luautils.stringStarts(item:getType(), 'Pills') then
+	elseif instanceof(item, 'Drainable') and luautils.stringStarts(item:getType(), 'Pills') then
 		ISInventoryPaneContextMenu.takePill(item, 0)
-	end
 
 	-- Umbrellas
-	if item:getName() == 'Umbrella' then
+	elseif item:getName() == 'Umbrella' then
 		ISInventoryPaneContextMenu.equipWeapon(item, false, false, 0)
 	end
 
