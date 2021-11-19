21131fb9ca7def9ac04653e3d42d56e5a5cff756
Corentin Delorme <codelorme@gmail.com>
2021-11-19 14:33:41 +0100

    Update QuickItemAction.lua: Refactoring

--- media/lua/client/QuickItemAction/QuickItemAction.lua
+++ media/lua/client/QuickItemAction/QuickItemAction.lua
@@ -13,6 +13,19 @@ function ISInventoryPane:doContextualDblClick(item)
 		ISInventoryPaneContextMenu.eatItem(item, 1, 0)
 	end
 
+	-- Keys
+	if item:getCategory() == 'Key' then
+		local player = getPlayer()
+		local containers = player:getInventory():getItemsFromCategory('Container')
+		for i = 0, containers:size() - 1 do
+			local container = containers:get(i)
+			if player:isEquipped(container) or container:getType() == 'KeyRing' then
+				container:getInventory():addItem(item)
+				break
+			end
+		end
+	end
+
 	-- Maps
 	if luautils.stringEnds(item:getType(), 'Map') then
 		ISInventoryPaneContextMenu.onCheckMap(item, 0)
@@ -26,19 +39,6 @@ function ISInventoryPane:doContextualDblClick(item)
 	-- Umbrellas
 	if item:getName() == 'Umbrella' then
 		ISInventoryPaneContextMenu.equipWeapon(item, false, false, 0)
-	end
-
-	-- Keys
-	if item:getCategory() == 'Key' then
-		local player = getPlayer()
-		local containers = player:getInventory():getItemsFromCategory('Container')
-		for i = 0, containers:size() - 1 do
-			local container = containers:get(i)
-			if player:isEquipped(container) or container:getType() == 'KeyRing' then
-				container:getInventory():addItem(item)
-				break
-			end
-		end
 	end
 
 	return origDoContextualDblClick(self, item);
