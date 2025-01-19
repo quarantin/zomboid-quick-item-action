aa4218911f1f2521920af0d43cc344cc50d1f566
Corentin Delorme <codelorme@gmail.com>
2025-01-19 22:24:42 +0100

    B42 compatibility + Handle more recipes (boxes etc)

--- media/lua/client/QuickItemAction/QuickItemAction.lua
+++ media/lua/client/QuickItemAction/QuickItemAction.lua
@@ -7,48 +7,120 @@ local pillsTypes = {
 	PillsVitamins        = true,
 }
 
+local cansTypes = {
+	BeerBottle           = true,
+	BeerCan              = true,
+	Pop                  = true,
+	Pop2                 = true,
+	Pop3                 = true,
+	PopBottle            = true,
+	SodaCan              = true,
+	TestCanPopCommon     = true,
+	Wine                 = true,
+}
+
+local handledRecipesTypes = {
+	AdhesiveBandageBox   = true,
+	AdhesiveTapeBox      = true,
+	AntibioticsBox       = true,
+	BandageBox           = true,
+	BatteryBox           = true,
+	BeerCanPack          = true,
+	BeerPack             = true,
+	BoxOfJars            = true,
+	CandleBox            = true,
+	CigarettePack        = true,
+	ColdpackBox          = true,
+	CottonBallsBox       = true,
+	DuctTapeBox          = true,
+	Garbagebag_box       = true,
+	FishingHookBox       = true,
+	LightBulbBox         = true,
+	SutureNeedleBox      = true,
+	TissueBox            = true,
+	TongueDepressorBox   = true,
+	WineRed_Boxed        = true,
+	WineWhite_Boxed      = true,
+}
+
 local function shouldHandleRecipe(itemName, itemCategory, itemDisplayCategory, itemType)
 
 	if itemCategory == 'Normal' then
-		return itemDisplayCategory == 'Ammo' or
-			luautils.stringStarts(itemType, 'Nails') or
-			luautils.stringStarts(itemType, 'Paperclip') or
-			luautils.stringStarts(itemType, 'Screws') or
-			luautils.stringEnds(itemType, 'Seed') or
-			luautils.stringEnds(itemType, 'Seeds')
+		return handledRecipesTypes[itemType]
+			or itemDisplayCategory == 'Ammo'
+			or luautils.stringStarts(itemType, 'Nails')
+			or luautils.stringStarts(itemType, 'Paperclip')
+			or luautils.stringStarts(itemType, 'Screws')
+			or luautils.stringEnds(itemType, 'Seed')
+			or luautils.stringEnds(itemType, 'Seeds')
+			or luautils.stringEnds(itemType, '_Box')
+			or (cansTypes[itemType] and string.find(itemName, '(Sealed)'))
+
+	elseif itemCategory == 'Drainable' then
+		return handledRecipesTypes[itemType]
+
+	elseif itemCategory == 'Literature' and itemDisplayCategory == 'Gardening' then
+		return luautils.stringEnds(itemType, 'BagSeed') or luautils.stringEnds(itemType, 'BagSeed2')
 	end
 end
 
-local function useRecipe(item, player)
-
-	local containers = ISInventoryPaneContextMenu.getContainers(player)
+local function useRecipe(player, playerNum, recipe, item, containers)
 
 	if getCore():getGameVersion():getMajor() < 42 then
 
-		local recipes = RecipeManager.getUniqueRecipeItems(item, player, containers)
-		if recipes and recipes:size() > 0 then
-
-			local recipe = recipes:get(0)
-			local howMany = RecipeManager.getNumberOfTimesRecipeCanBeDone(recipe, player, containers, item)
-			if howMany < 1 then
-				return
-			end
-
+		local howMany = RecipeManager.getNumberOfTimesRecipeCanBeDone(recipe, player, containers, item)
+		if howMany > 0 then
 			local action = ISCraftAction:new(player, item, recipe:getTimeToMake(), recipe, inventory, containers)
 			ISTimedActionQueue.add(action)
 		end
-
 	else
-
-		local recipes = CraftRecipeManager.getUniqueRecipeItems(item, player, containers)
-		if recipes and recipes:size() > 0 then
-
-			local recipe = recipes:get(0)
-			ISInventoryPaneContextMenu.OnNewCraft(item, recipe, player:getIndex(), false)
-		end
+		ISInventoryPaneContextMenu.OnNewCraft(item, recipe, playerNum, false)
 
 	end
 end
+
+local function getRecipes(item, player, containers)
+
+	if getCore():getGameVersion():getMajor() < 42 then
+		return RecipeManager.getUniqueRecipeItems(item, player, containers)
+	else
+		return CraftRecipeManager.getUniqueRecipeItems(item, player, containers)
+	end
+end
+
+local function findRecipeByName(recipeName, recipes)
+	for i = 0, recipes:size() - 1 do
+		local recipe = recipes:get(i)
+		if recipe:getName() == recipeName then
+			return recipe
+		end
+	end
+end
+
+local function selectRecipe(itemType, recipes)
+
+	local recipe
+
+	-- The first recipe is either `SmashBottle` or `PackBeerBottles`
+	if itemType == 'BeerBottle' then
+		recipe = findRecipeByName('OpenBottleOfBeer', recipes)
+
+	-- The first recipe may be `AddACigarette`
+	elseif itemType == 'CigarettePack' then
+		recipe = findRecipeByName('TakeACigarette', recipes)
+
+	-- The first recipe may be `Read`
+	elseif luautils.stringEnds(itemType, 'BagSeed') or luautils.stringEnds(itemType, 'BagSeed2') then
+		recipe = findRecipeByName('OpenPacketOfSeeds', recipes)
+	end
+
+	if recipe then
+		return recipe
+	end
+
+	return recipes:get(0)
+end
+
 
 local origDoContextualDblClick = ISInventoryPane.doContextualDblClick;
 
@@ -74,7 +146,7 @@ function ISInventoryPane:doContextualDblClick(item)
 	end
 
 	-- Books
-	if itemCategory == 'Literature' then
+	if itemCategory == 'Literature' and itemDisplayCategory ~= 'Gardening' then
 		ISInventoryPaneContextMenu.readItem(item, playerNum)
 
 	-- Cigarettes
@@ -93,6 +165,10 @@ function ISInventoryPane:doContextualDblClick(item)
 			end
 		end
 
+	-- Liquids
+	elseif cansTypes[itemType] and not string.find(itemName, '(Sealed)') and not string.find(itemName, 'Empty') then
+		ISInventoryPaneContextMenu.onDrinkFluid(item, 1, player)
+
 	-- Maps
 	elseif itemCategory == 'Map' or itemDisplayCategory == 'Cartography' then
 		ISInventoryPaneContextMenu.onCheckMap(item, playerNum)
@@ -107,9 +183,14 @@ function ISInventoryPane:doContextualDblClick(item)
 
 	-- Use Recipes
 	elseif shouldHandleRecipe(itemName, itemCategory, itemDisplayCategory, itemType) then
-		useRecipe(item, player)
 
+		local containers = ISInventoryPaneContextMenu.getContainers(player)
+		local recipes = getRecipes(item, player, containers)
 
+		if recipes and recipes:size() > 0 then
+			local recipe = selectRecipe(itemType, recipes)
+			useRecipe(player, playerNum, recipe, item, containers)
+		end
 	end
 
 	return origDoContextualDblClick(self, item);
